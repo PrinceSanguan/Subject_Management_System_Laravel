ecb76f30a3c454814eb88623c3418f8eb4a77499
Prince Sanguan <princesanguan44@gmail.com>
2024-06-17 21:29:34 +0800

    done in login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [LoginController::class, 'index'])->name('login');
+
+Route::get('signup', [SignupController::class, 'index'])->name('signup');
+
